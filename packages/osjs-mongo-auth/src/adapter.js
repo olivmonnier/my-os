2ccefd03c77ea512bb9c7ec1211080981a6b8e45
Olivier MONNIER <monnier@indigo-net.com>
2019-01-07 13:32:25 +0100

    fix auth adapter

--- packages/osjs-mongo-auth/src/adapter.js
+++ packages/osjs-mongo-auth/src/adapter.js
@@ -7,15 +7,18 @@ module.exports = (core, options = {}) => {
   return {
     logout: () => Promise.resolve(true),
     login: async (req, res) => {
-      const { username, password } = req.body;
-      const user = await findUser(url, dbName)(username);
-
-      
-      if (user) {
-        user['id'] = user._id;
-        return comparePassword(password, user.password)
-          .then(result => result ? user : false);
-      } else return false;
+      try {
+        const { username, password } = req.body;
+        const user = await findUser(url, dbName)(username);
+        
+        if (user) {
+          user['id'] = user._id;
+          return comparePassword(password, user.password)
+            .then(result => result ? user : false);
+        } else return false;
+      } catch(err) {
+        return err
+      }
     }
   }
 }
